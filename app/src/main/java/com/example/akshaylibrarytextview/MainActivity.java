0d36dfb5c0955e966759b8211811a40354fa4e7e
AkshayPapneja <akshypapnjs@gmail.com>
2019-06-06 17:18:58 +0530

    Activate Library

---
+++ app/src/main/java/com/example/akshaylibrarytextview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.akshaylibrarytextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.akshypapnjsverticalandhorizontaltextview.AkshayPapnjsVerticalTextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
